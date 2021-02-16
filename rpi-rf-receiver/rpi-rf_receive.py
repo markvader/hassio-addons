86c44be7ec84fb735155ce17e9e9ab3aa830ecf9
psallandre <patrice@sallandre.com>
2021-02-16 15:49:58 +0100

    rpi-rf_receive.py: - add MQTT, valid-codes, log-level parameters - fix throttling repeated codes

--- rpi-rf-receiver/rpi-rf_receive.py
+++ rpi-rf-receiver/rpi-rf_receive.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import argparse
 import signal
 import sys
@@ -7,45 +6,57 @@ import time
 import logging
 import io, os, shutil
 from datetime import datetime
-
-mosquitto_address = "core.mosquitto"
-mosquitto_port = "1883"
-mosquitto_user = ""
-mosquitto_password = ""
-
 from rpi_rf import RFDevice
 
 rfdevice = None
-valid_codes = []        # Device n°1 codes as integer
-valid_codes.extend([])  # Device n°2 codes as integer
 
 # pylint: disable=unused-argument
 def exithandler(signal, frame):
     rfdevice.cleanup()
     sys.exit(0)
 
-logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
-                    format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s', )
+levels = {
+    'trace': logging.DEBUG,
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'notice': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+    'fatal': logging.CRITICAL
+}
 
 parser = argparse.ArgumentParser(description='Receives a decimal code via a 433/315MHz GPIO device')
-parser.add_argument('-g', dest='gpio', type=int, default=27,
-                    help="GPIO pin (Default: 27)")
+parser.add_argument('--valid-codes', nargs='*', type=int)
+parser.add_argument('--log-level', choices=list(levels.keys()), help="Provide logging level")
+parser.add_argument('-g', dest='gpio', type=int, default=27, help="GPIO pin (Default: 27)")
+parser.add_argument('--mqtt-host', required=True)
+parser.add_argument('--mqtt-port', required=True)
+parser.add_argument('--mqtt-user', required=True)
+parser.add_argument('--mqtt-password', required=True)
 args = parser.parse_args()
+
+logging.basicConfig(level=levels.get(args.log_level), datefmt='%Y-%m-%d %H:%M:%S', format='[%(asctime)-15s] %(levelname)s %(module)s: %(message)s', )
+logging.debug(f"rf codes to listen for: {args.valid_codes}")
 
 signal.signal(signal.SIGINT, exithandler)
 rfdevice = RFDevice(args.gpio)
 rfdevice.enable_rx()
+
 timestamp = None
-last_sent = datetime.now()
+last_code = 0
+last_sent = datetime.min
+command=f"mosquitto_pub -V mqttv311 -h {args.mqtt_host} -p {args.mqtt_port} -t 'sensors/rf/receiver2' -u {args.mqtt_user} -P {args.mqtt_password}"
 logging.info("Listening for codes on GPIO " + str(args.gpio))
 while True:
-    seconds_since_last_mqtt_message_sent = (datetime.now() - last_sent).total_seconds()
-    if rfdevice.rx_code_timestamp != timestamp and rfdevice.rx_code in valid_codes and seconds_since_last_mqtt_message_sent > 3:
-        timestamp = rfdevice.rx_code_timestamp
-        logging.info(str(rfdevice.rx_code) +
-                     " [pulselength " + str(rfdevice.rx_pulselength) +
-                     ", protocol " + str(rfdevice.rx_proto) + "]")
-        os.system("mosquitto_pub -V mqttv311 -h " + mosquitto_address + " -p " + mosquitto_port + " -t 'sensors/rf/receiver' -u " + mosquitto_user + " -P " + mosquitto_password + " -m " + str(rfdevice.rx_code))
-        last_sent = datetime.now()
-    time.sleep(0.01)
+    if rfdevice.rx_code_timestamp != timestamp:
+        if not args.valid_codes or rfdevice.rx_code in args.valid_codes:
+            if rfdevice.rx_code != last_code or (datetime.now() - last_sent).total_seconds() > 3:
+                timestamp = rfdevice.rx_code_timestamp
+                logging.info(f"{rfdevice.rx_code} [pulselength {rfdevice.rx_pulselength}, protocol {rfdevice.rx_proto}]")
+                os.system(f"{command} -m {rfdevice.rx_code}")
+                last_code = rfdevice.rx_code
+                last_sent = datetime.now()
+        else:
+            logging.debug(f"received unvalid code: {rfdevice.rx_code}")
+    time.sleep(0.5)
 rfdevice.cleanup()
