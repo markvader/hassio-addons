2f66535dc7f5fe40895e31214049810d6fe0b446
psallandre <patrice@sallandre.com>
2021-04-06 21:52:29 +0200

    [mqtt2rf] MQTT 2 RF

---
+++ mqtt2rf/README.md
@@ -0,0 +1,5 @@
+# MQTT 2 RF
+
+This is not meant to be run as an Home Assistant addon (for Home Assistant [Raspberry Pi RF](https://www.home-assistant.io/integrations/rpi_rf/) is OK )
+
+We are just reusing addon 'architecture', to run `mqtt2rf.py` as a docker container in Raspberry OS.
---
+++ mqtt2rf/rootfs/usr/bin/mqtt2rf.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# https://pypi.org/project/paho-mqtt/
+# https://github.com/milaq/rpi-rf/blob/master/scripts/rpi-rf_send
+# https://github.com/home-assistant/core/blob/dev/homeassistant/components/rpi_rf/switch.py
+import argparse
+import signal
+import sys
+import time
+import logging
+import io, os, shutil
+import paho.mqtt.client as mqtt
+from datetime import datetime
+from rpi_rf import RFDevice
+
+PROTOCOL = 1
+PULSELENGTH = 350
+MQTT_KEEPALIVE_S = 120
+rfdevice = None
+
+# pylint: disable=unused-argument
+def exithandler(signal, frame):
+    rfdevice.cleanup()
+    sys.exit(0)
+
+
+levels = {
+    "trace": logging.DEBUG,
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "notice": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "fatal": logging.CRITICAL,
+}
+
+parser = argparse.ArgumentParser(description="Receives a decimal code via a 433/315MHz GPIO device")
+parser.add_argument("--log-level", choices=list(levels.keys()), help="Provide logging level", default="info")
+parser.add_argument("-g", dest="gpio", type=int, default=27, help="GPIO pin (Default: 27)")
+parser.add_argument("--mqtt-host", required=True)
+parser.add_argument("--mqtt-port", required=True)
+parser.add_argument("--mqtt-user", required=True)
+parser.add_argument("--mqtt-password", required=True)
+parser.add_argument("--mqtt-test", default=None)
+args = parser.parse_args()
+
+logging.basicConfig(
+    level=levels.get(args.log_level),
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="[%(asctime)-15s] %(levelname)s %(module)s: %(message)s",
+)
+
+signal.signal(signal.SIGINT, exithandler)
+rfdevice = RFDevice(args.gpio)
+rfdevice.enable_tx()
+
+
+def on_connect(client, userdata, flags, rc):
+    logging.info(f"Connected with result code {rc}")
+    if args.mqtt_test:
+        logging.info("Sending MQTT test messsage to 'test/topic'")
+        client.publish("test/topic", "hello from mqtt2rf")
+    logging.info("Subscribing to 'switches/rf'")
+    client.subscribe("switches/rf/#")
+
+
+def on_message(client, userdata, message):
+    logging.debug(f"message: userdata '{userdata}' message payload '{str(message.payload)}' topic '{message.topic}'")
+    try:
+        code = int(message.topic.split("/")[-1])
+        logging.info(f"rf send '{code}' [pulselength {PULSELENGTH}, protocol {PROTOCOL}]")
+        rfdevice.tx_code(code, PROTOCOL, PULSELENGTH)
+    except Exception as e:
+        logging.error(f"exception: '{repr(e)}'")
+
+
+def on_log(client, obj, level, buf):
+    logging.debug(f"log: level {level} message '{str(buf)}' userdata '{str(obj)}'")
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_log = on_log
+client.username_pw_set(args.mqtt_user, args.mqtt_password)
+client.connect(args.mqtt_host, int(args.mqtt_port), MQTT_KEEPALIVE_S)
+client.loop_forever(retry_first_connection=True)
