10678cd45d19874e5a48e414d922b98a75c516ae
psallandre <patrice@sallandre.com>
2021-04-06 21:45:49 +0200

    [rpi-rf-receive] format code

--- rpi-rf-receiver/rootfs/usr/bin/rpi-rf-receive.py
+++ rpi-rf-receiver/rootfs/usr/bin/rpi-rf-receive.py
@@ -15,28 +15,33 @@ def exithandler(signal, frame):
     rfdevice.cleanup()
     sys.exit(0)
 
+
 levels = {
-    'trace': logging.DEBUG,
-    'debug': logging.DEBUG,
-    'info': logging.INFO,
-    'notice': logging.INFO,
-    'warning': logging.WARNING,
-    'error': logging.ERROR,
-    'fatal': logging.CRITICAL
+    "trace": logging.DEBUG,
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "notice": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "fatal": logging.CRITICAL,
 }
 
-parser = argparse.ArgumentParser(description='Receives a decimal code via a 433/315MHz GPIO device')
-parser.add_argument('--valid-codes', nargs='*', type=int)
-parser.add_argument('--log-level', choices=list(levels.keys()), help="Provide logging level")
-parser.add_argument('-g', dest='gpio', type=int, default=27, help="GPIO pin (Default: 27)")
-parser.add_argument('--mqtt-host', required=True)
-parser.add_argument('--mqtt-port', required=True)
-parser.add_argument('--mqtt-user', required=True)
-parser.add_argument('--mqtt-password', required=True)
-parser.add_argument('--mqtt-test', required=True)
+parser = argparse.ArgumentParser(description="Receives a decimal code via a 433/315MHz GPIO device")
+parser.add_argument("--valid-codes", nargs="*", type=int)
+parser.add_argument("--log-level", choices=list(levels.keys()), help="Provide logging level")
+parser.add_argument("-g", dest="gpio", type=int, default=27, help="GPIO pin (Default: 27)")
+parser.add_argument("--mqtt-host", required=True)
+parser.add_argument("--mqtt-port", required=True)
+parser.add_argument("--mqtt-user", required=True)
+parser.add_argument("--mqtt-password", required=True)
+parser.add_argument("--mqtt-test", required=True)
 args = parser.parse_args()
 
-logging.basicConfig(level=levels.get(args.log_level), datefmt='%Y-%m-%d %H:%M:%S', format='[%(asctime)-15s] %(levelname)s %(module)s: %(message)s', )
+logging.basicConfig(
+    level=levels.get(args.log_level),
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="[%(asctime)-15s] %(levelname)s %(module)s: %(message)s",
+)
 logging.debug(f"rf codes to listen for: {args.valid_codes}")
 
 if args.mqtt_test:
@@ -50,7 +55,7 @@ rfdevice.enable_rx()
 timestamp = None
 last_code = 0
 last_sent = datetime.min
-command=f"mosquitto_pub -V mqttv311 -h {args.mqtt_host} -p {args.mqtt_port} -t 'sensors/rf/receiver' -u {args.mqtt_user} -P {args.mqtt_password}"
+command = f"mosquitto_pub -V mqttv311 -h {args.mqtt_host} -p {args.mqtt_port} -t 'sensors/rf/receiver' -u {args.mqtt_user} -P {args.mqtt_password}"
 logging.info("Listening for codes on GPIO " + str(args.gpio))
 while True:
     if rfdevice.rx_code_timestamp != timestamp:
